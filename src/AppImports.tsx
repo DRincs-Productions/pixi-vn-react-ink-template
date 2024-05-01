02d8afab3badfcb7132bbb703c72d604284175f8
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-01 18:28:22 +0200

    Update @drincs/nqtr dependency to version 0.0.2

--- src/AppImports.tsx
+++ src/AppImports.tsx
@@ -1,3 +1,6 @@
+import { TimeManager } from '@drincs/nqtr';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import MyThemeProvider from './providers/ThemeProvider';
@@ -7,6 +10,24 @@ type Iprops = {
 }
 
 export default function AppImports(props: Iprops) {
+    const { t } = useTranslation(["translation"]);
+    useEffect(() => {
+        TimeManager.settings = {
+            defaultTimeSpent: 1,
+            maxDayHours: 24,
+            minDayHours: 0,
+            timeSlots: [
+                { name: t('morning'), startHour: 5 },
+                { name: t('afternoon'), startHour: 12 },
+                { name: t('evening'), startHour: 18 },
+                { name: t('night'), startHour: 22 }
+            ],
+            weekDaysNames: [t('monday'), t('tuesday'), t('wednesday'), t('thursday'), t('friday'), t('saturday'), t('sunday')],
+            weekendStartDay: 6,
+            weekLength: 7,
+        }
+    }, []);
+
     return (
         <BrowserRouter>
             <RecoilRoot>
