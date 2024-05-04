ab2280a85b2f449c219794207a3b10f02267e6a3
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-04 15:32:59 +0200

    currentLocationCommitmentsState

--- src/label/StartLabel.ts
+++ src/label/StartLabel.ts
@@ -1,27 +1,14 @@
-import { ChoiceMenuOptionLabel, GameStepManager, GameWindowManager, Label, labelDecorator, setChoiceMenuOptions, setDialogue, StepLabelType } from "@drincs/pixi-vn";
-import { liam } from "../values/characters";
-import { BaseCanvasElementTestLabel } from "./BaseCanvasElementTestLabel";
-import { EventsTestLabel } from "./EventsTestLabel";
-import { ShowImageTest } from "./ShowImageTest";
-import { TickerTestLabel } from "./TickerTestLabel";
-import { TintingTestLabel } from "./TintingTestLabel";
+import { Label, labelDecorator, StepLabelType } from "@drincs/pixi-vn";
 
 @labelDecorator()
 export class StartLabel extends Label {
     override get steps(): StepLabelType[] {
         return [
             () => {
-                GameWindowManager.clear()
-                setDialogue({ character: liam, text: "Which test do you want to perform?" })
-                setChoiceMenuOptions([
-                    new ChoiceMenuOptionLabel("Events Test", EventsTestLabel),
-                    new ChoiceMenuOptionLabel("Show Image Test", ShowImageTest),
-                    new ChoiceMenuOptionLabel("Ticker Test", TickerTestLabel),
-                    new ChoiceMenuOptionLabel("Tinting Test", TintingTestLabel),
-                    new ChoiceMenuOptionLabel("Base Canvas Element Test Label", BaseCanvasElementTestLabel)
-                ])
+                return {
+                    newRoute: "/navigation",
+                }
             },
-            () => GameStepManager.jumpLabel(StartLabel),
         ]
     }
 }
