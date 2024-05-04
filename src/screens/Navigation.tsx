d68fd2da3fdb397cbdbfe94a766f18842ad70261
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-04 21:56:08 +0200

    feat: Add currentLocationCommitmentsState to Navigation screen

--- src/screens/Navigation.tsx
+++ src/screens/Navigation.tsx
@@ -1,7 +1,9 @@
 import { getCurrenrLocation, getCurrentCommitments, getCurrentRoom } from '@drincs/nqtr';
+import { GameStorageManager } from '@drincs/pixi-vn';
 import { Grid } from '@mui/joy';
 import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
+import { currentLocationCommitmentsState } from '../atoms/currentLocationCommitmentsState';
 import { currentLocationState } from '../atoms/currentLocationState';
 import { currentRoomState } from '../atoms/currentRoomState';
 import TextMenuButton from '../components/TextMenuButton';
@@ -9,9 +11,11 @@ import TextMenuButton from '../components/TextMenuButton';
 export default function Navigation() {
     const [currentLocation, setCurrentLocation] = useRecoilState(currentLocationState)
     const [currentRoom, setCurrentRoom] = useRecoilState(currentRoomState)
+    const [currentLocationCommitments, setCurrentLocationCommitments] = useRecoilState(currentLocationCommitmentsState)
 
     useEffect(() => {
         let location = getCurrenrLocation()
+        console.log(GameStorageManager)
         if (location) {
             setCurrentLocation(location)
         }
@@ -24,7 +28,10 @@ export default function Navigation() {
         let locationCommitments = getCurrentCommitments().filter((commitment) => {
             return commitment.room.location.id === currentLocation?.id
         })
+        setCurrentLocationCommitments(locationCommitments)
     }, [currentLocation])
+    console.log(currentRoom)
+    console.log(currentLocationCommitments)
 
     return (
         <>
