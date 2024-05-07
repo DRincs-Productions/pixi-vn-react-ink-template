07fc3b5d22405644f431a74cfe5928cff0341619
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-07 08:48:32 +0200

    refactor: Update DialogueModel to store i18nArgs in oltherParams

--- src/model/DialogueModel.ts
+++ src/model/DialogueModel.ts
@@ -1,14 +1,23 @@
-import { DialogueBaseModel } from "@drincs/pixi-vn";
+import { DialogueBaseModel, StorageElementType } from "@drincs/pixi-vn";
 import { CharacterModel } from "./characters/CharacterModel";
 
 export class DialogueModel extends DialogueBaseModel {
     constructor(
         text: string,
         character: CharacterModel | string,
-        i18nArgs: { [key: string]: string }
+        i18nArgs: { [key: string]: string } = {}
     ) {
         super(text, character);
-        this.i18nArgs = i18nArgs;
+        this.oltherParams = {
+            i18nArgs: i18nArgs
+        }
     }
-    i18nArgs: { [key: string]: string } = {}
+    oltherParams: {
+        [key: string | number | symbol]: StorageElementType,
+        i18nArgs: { [key: string]: string }
+    }
+
+    get i18nArgs() {
+        return this.oltherParams.i18nArgs;
+    }
 }
