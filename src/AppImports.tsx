6acb6ee9a9212049a1e6fd8be2bf230198951e3a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-07 08:54:09 +0200

    refactor: Update AppImports to use localized time slot descriptions

--- src/AppImports.tsx
+++ src/AppImports.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import MyThemeProvider from './providers/ThemeProvider';
+import { timeSlots } from './values/constants';
 
 type Iprops = {
     children: React.ReactNode
@@ -17,10 +18,10 @@ export default function AppImports(props: Iprops) {
             maxDayHours: 24,
             minDayHours: 0,
             timeSlots: [
-                { name: t('morning'), startHour: 5 },
-                { name: t('afternoon'), startHour: 12 },
-                { name: t('evening'), startHour: 18 },
-                { name: t('night'), startHour: 22 }
+                { name: t(timeSlots.morning.description), startHour: timeSlots.morning.value },
+                { name: t(timeSlots.afternoon.description), startHour: timeSlots.afternoon.value },
+                { name: t(timeSlots.evening.description), startHour: timeSlots.evening.value },
+                { name: t(timeSlots.night.description), startHour: timeSlots.night.value },
             ],
             weekDaysNames: [t('monday'), t('tuesday'), t('wednesday'), t('thursday'), t('friday'), t('saturday'), t('sunday')],
             weekendStartDay: 6,
