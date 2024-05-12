cb9e2a05d004f91b5055c998be83b0f5c4487070
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-12 18:43:31 +0200

    refactor: Remove unused code and update AppImports to use localized time slot descriptions

--- src/AppImports.tsx
+++ src/AppImports.tsx
@@ -1,34 +1,12 @@
-import { TimeManager } from '@drincs/nqtr';
-import { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import MyThemeProvider from './providers/ThemeProvider';
-import { timeSlots } from './values/constants';
 
 type Iprops = {
     children: React.ReactNode
 }
 
 export default function AppImports(props: Iprops) {
-    const { t } = useTranslation(["translation"]);
-    useEffect(() => {
-        TimeManager.settings = {
-            defaultTimeSpent: 1,
-            maxDayHours: 24,
-            minDayHours: 0,
-            timeSlots: [
-                { name: t(timeSlots.morning.description), startHour: timeSlots.morning.value },
-                { name: t(timeSlots.afternoon.description), startHour: timeSlots.afternoon.value },
-                { name: t(timeSlots.evening.description), startHour: timeSlots.evening.value },
-                { name: t(timeSlots.night.description), startHour: timeSlots.night.value },
-            ],
-            weekDaysNames: [t('monday'), t('tuesday'), t('wednesday'), t('thursday'), t('friday'), t('saturday'), t('sunday')],
-            weekendStartDay: 6,
-            weekLength: 7,
-        }
-    }, []);
-
     return (
         <BrowserRouter>
             <RecoilRoot>
