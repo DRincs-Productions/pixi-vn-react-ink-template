921757ab2dcfadf902cc8a3ac541d3b22c79270b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-12 18:43:43 +0200

    refactor: Update mcHome icon in locations.tsx

--- src/values/locations.tsx
+++ src/values/locations.tsx
@@ -1,8 +1,11 @@
 import { LocationBaseModel } from "@drincs/nqtr";
-import { CanvasSprite } from "@drincs/pixi-vn";
 import { mainMap } from "./maps";
 
 export const mcHome = new LocationBaseModel('mc_home', mainMap, {
     name: 'MC Home',
-    icon: new CanvasSprite()
+    // icon: new CanvasImage({
+    //     x: 100,
+    //     y: 100,
+    //     texture: Texture.EMPTY,
+    // }, "https://cdn-icons-png.freepik.com/512/5426/5426899.png?ga=GA1.1.2068448463.1715274700")
 });
