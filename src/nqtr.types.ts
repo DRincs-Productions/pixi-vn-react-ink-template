dd278110c4dfe9c866f9baf3d2cea2c5efce6988
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-06-02 22:42:41 +0200

    chore: Update @drincs/nqtr dependency to version 0.1.17

--- src/nqtr.types.ts
+++ src/nqtr.types.ts
@@ -2,7 +2,7 @@ import { StepLabelProps } from "@drincs/pixi-vn/dist/override";
 import { ImageTimeSlots } from "./model/TimeSlots";
 
 declare module '@drincs/nqtr/dist/override' {
-    interface OnRunActivityProps extends StepLabelProps {
+    interface OnRunProps extends StepLabelProps {
     }
     interface OnRenderGraphicItemProps extends StepLabelProps {
     }
