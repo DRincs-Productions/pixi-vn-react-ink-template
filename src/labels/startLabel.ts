d983bbf90565db82e4d9bd39e8139e82f6afa993
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-06-19 09:11:37 +0200

    refactor: Update startLabel to use more descriptive navigation label IDs

--- src/labels/startLabel.ts
+++ src/labels/startLabel.ts
@@ -5,8 +5,8 @@ import { timeSlots } from "../values/constants";
 import { mcRoom } from "../values/rooms";
 
 const START_LABEL_ID = "start_label_id"
-const GO_TO_NAVIGATION_LABEL_ID = "go_to_navigation_label_id"
-const GO_TO_NAVIGATION_LABEL_ID2 = "go_to_navigation_label_id2"
+const GO_TO_NAVIGATION_FATHER_LABEL_ID = "go_to_navigation_label_id"
+const GO_TO_NAVIGATION_LABEL_ID = "go_to_navigation_label_id2"
 
 export const startLabel = newLabel(START_LABEL_ID,
     [
@@ -29,7 +29,7 @@ export const startLabel = newLabel(START_LABEL_ID,
             setFlag("weekend", TimeManager.isWeekend)
             setFlag("not_weekend", !TimeManager.isWeekend)
             aliceQuest.start({})
-            GameStepManager.callLabel(goToNavigationThatPreventsJump, props)
+            GameStepManager.callLabel(goToNavigationFather, props)
         },
         ({ navigate }) => {
             console.error("There was an error in the game.")
@@ -38,20 +38,28 @@ export const startLabel = newLabel(START_LABEL_ID,
     ]
 )
 
-const goToNavigationThatPreventsJump = newLabel(GO_TO_NAVIGATION_LABEL_ID,
+/**
+ * This label ensures that even when {@link goToNavigation} is closed, {@link goToNavigation} is reopened.
+ */
+const goToNavigationFather = newLabel(GO_TO_NAVIGATION_FATHER_LABEL_ID,
     () => {
         return [
             (props) => GameStepManager.callLabel(goToNavigation, props),
-            (props) => GameStepManager.jumpLabel(GO_TO_NAVIGATION_LABEL_ID, props),
+            (props) => GameStepManager.jumpLabel(GO_TO_NAVIGATION_FATHER_LABEL_ID, props),
         ]
     }
 )
 
-const goToNavigation = newLabel(GO_TO_NAVIGATION_LABEL_ID2,
+/**
+ * This label causes the player to return to navigation after completing a dialogue.
+ * This label creates a loop, causing the game to never end.
+ * Only if a jump is called immediately after this label, it will be terminated, moving on to the father label.
+ */
+const goToNavigation = newLabel(GO_TO_NAVIGATION_LABEL_ID,
     () => {
         return [
             ({ navigate }) => navigate('/navigation'),
-            (props) => GameStepManager.jumpLabel(goToNavigationThatPreventsJump, props),
+            (props) => GameStepManager.jumpLabel(GO_TO_NAVIGATION_FATHER_LABEL_ID, props),
         ]
     }
 )
