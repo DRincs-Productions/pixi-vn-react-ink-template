f2d44af6f2676b5fa59f86a99af6d0401a1804d9
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-06-20 17:04:04 +0200

    Update

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,19 +1,27 @@
-import { CharacterBaseModel, getCharacterById, getDialogueHistory } from '@drincs/pixi-vn';
+import { CharacterBaseModel, DialogueHistory, getCharacterById, getDialogueHistory } from '@drincs/pixi-vn';
 import { ModalDialogExtended } from '@drincs/react-components';
 import CheckIcon from '@mui/icons-material/Check';
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import { Box, Chip, Input, Stack, Typography } from "@mui/joy";
 import Avatar from '@mui/joy/Avatar';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { useRecoilState } from 'recoil';
-import { openHistoryState } from '../atoms/openHistoryState';
 import { DialogueModel } from '../model/DialogueModel';
 
 export default function History() {
-    const [open, setOpen] = useRecoilState(openHistoryState);
-    const [searchString, setSearchString] = useState("")
     const { t } = useTranslation(["translation"]);
+    const methods = useForm<{
+        open: boolean,
+        searchString: string,
+        history: DialogueHistory<DialogueModel>[]
+    }>({
+        defaultValues: {
+            open: false,
+            history: [],
+            searchString: "",
+        }
+    });
 
     useEffect(() => {
         window.addEventListener('keydown', onkeydown);
@@ -24,111 +32,118 @@ export default function History() {
 
     function onkeydown(event: KeyboardEvent) {
         if (event.code == 'KeyH') {
-            setOpen((prev) => !prev)
+            let open = methods.getValues("open")
+            methods.setValue("open", !open)
         }
     }
 
     return (
-        <ModalDialogExtended
-            open={open}
-            setOpen={setOpen}
-            head={<Stack
-                sx={{
-                    width: "100%",
-                }}
-            >
-                <Stack sx={{ mb: 2 }}>
-                    <Typography level="h2">
-                        {t("history")}
-                    </Typography>
-                </Stack>
-                <Input
-                    placeholder={t("search")}
-                    value={searchString}
-                    onChange={(e) => setSearchString(e.target.value)}
-                    startDecorator={<SearchRoundedIcon />}
-                    aria-label={t("search")}
-                />
-            </Stack>}
-            minWidth="80%"
-            sx={{
-                maxHeight: "80%",
-            }}
-        >
-            <Box
-                sx={{
-                    display: 'flex',
-                    flex: 1,
-                    minHeight: 0,
-                    px: 2,
-                    py: 3,
-                    overflowY: 'scroll',
-                    flexDirection: 'column-reverse',
-                }}
-            >
-                <Stack spacing={2} justifyContent="flex-end">
-                    {getDialogueHistory<DialogueModel>()
-                        .map((step) => {
-                            let character = step.dialoge?.characterId ? getCharacterById(step.dialoge?.characterId) ?? new CharacterBaseModel(step.dialoge?.characterId, { name: t(step.dialoge?.characterId) }) : undefined
-                            return {
-                                character: character?.name ? character.name + (character.surname ? " " + character.surname : "") : undefined,
-                                text: step.dialoge?.text || "",
-                                icon: character?.icon,
-                                choices: step.choices?.map((choice) => {
+        <Controller
+            control={methods.control}
+            name="open"
+            render={({ field: { value: open } }) => (
+                <ModalDialogExtended
+                    open={open}
+                    setOpen={setOpen}
+                    head={<Stack
+                        sx={{
+                            width: "100%",
+                        }}
+                    >
+                        <Stack sx={{ mb: 2 }}>
+                            <Typography level="h2">
+                                {t("history")}
+                            </Typography>
+                        </Stack>
+                        <Input
+                            placeholder={t("search")}
+                            value={searchString}
+                            onChange={(e) => setSearchString(e.target.value)}
+                            startDecorator={<SearchRoundedIcon />}
+                            aria-label={t("search")}
+                        />
+                    </Stack>}
+                    minWidth="80%"
+                    sx={{
+                        maxHeight: "80%",
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flex: 1,
+                            minHeight: 0,
+                            px: 2,
+                            py: 3,
+                            overflowY: 'scroll',
+                            flexDirection: 'column-reverse',
+                        }}
+                    >
+                        <Stack spacing={2} justifyContent="flex-end">
+                            {getDialogueHistory<DialogueModel>()
+                                .map((step) => {
+                                    let character = step.dialoge?.characterId ? getCharacterById(step.dialoge?.characterId) ?? new CharacterBaseModel(step.dialoge?.characterId, { name: t(step.dialoge?.characterId) }) : undefined
                                     return {
-                                        text: choice.text,
-                                        isResponse: choice.isResponse,
+                                        character: character?.name ? character.name + (character.surname ? " " + character.surname : "") : undefined,
+                                        text: step.dialoge?.text || "",
+                                        icon: character?.icon,
+                                        choices: step.choices?.map((choice) => {
+                                            return {
+                                                text: choice.text,
+                                                isResponse: choice.isResponse,
+                                            }
+                                        })
                                     }
                                 })
-                            }
-                        })
-                        .filter((data) => {
-                            if (!searchString) return true
-                            return data.character?.toLowerCase().includes(searchString.toLowerCase()) || data.text?.toLowerCase().includes(searchString.toLowerCase())
-                        })
-                        .map((data, index) => {
-                            return <React.Fragment key={"history" + index}>
-                                <Stack
-                                    direction="row"
-                                    spacing={1.5}
-                                >
-                                    <Avatar
-                                        size="sm"
-                                        src={data.icon}
-                                    />
-                                    <Box sx={{ flex: 1 }}>
-                                        {data.character && <Typography level="title-sm">{data.character}</Typography>}
-                                        <Typography level="body-sm">{data.text}</Typography>
-                                    </Box>
-                                </Stack>
-                                <Stack
-                                    direction="row"
-                                    spacing={0.5}
-                                >
-                                    <Box sx={{ flex: 1 }}>
-                                        {data.choices && data.choices.map((choice, index) => {
-                                            if (choice.isResponse) {
-                                                return <Chip
-                                                    key={"choices-success" + index}
-                                                    color="success"
-                                                    endDecorator={<CheckIcon />}
-                                                >
-                                                    {choice.text}
-                                                </Chip>
-                                            }
-                                            return <Chip
-                                                key={"choices" + index}
-                                                color="primary"
-                                            >
-                                                {choice.text}
-                                            </Chip>
-                                        })}
-                                    </Box>
-                                </Stack>
-                            </React.Fragment>
-                        })}
-                </Stack>
-            </Box>
-        </ModalDialogExtended>
-    );
+                                .filter((data) => {
+                                    if (!searchString) return true
+                                    return data.character?.toLowerCase().includes(searchString.toLowerCase()) || data.text?.toLowerCase().includes(searchString.toLowerCase())
+                                })
+                                .map((data, index) => {
+                                    return <React.Fragment key={"history" + index}>
+                                        <Stack
+                                            direction="row"
+                                            spacing={1.5}
+                                        >
+                                            <Avatar
+                                                size="sm"
+                                                src={data.icon}
+                                            />
+                                            <Box sx={{ flex: 1 }}>
+                                                {data.character && <Typography level="title-sm">{data.character}</Typography>}
+                                                <Typography level="body-sm">{data.text}</Typography>
+                                            </Box>
+                                        </Stack>
+                                        <Stack
+                                            direction="row"
+                                            spacing={0.5}
+                                        >
+                                            <Box sx={{ flex: 1 }}>
+                                                {data.choices && data.choices.map((choice, index) => {
+                                                    if (choice.isResponse) {
+                                                        return <Chip
+                                                            key={"choices-success" + index}
+                                                            color="success"
+                                                            endDecorator={<CheckIcon />}
+                                                        >
+                                                            {choice.text}
+                                                        </Chip>
+                                                    }
+                                                    return <Chip
+                                                        key={"choices" + index}
+                                                        color="primary"
+                                                    >
+                                                        {choice.text}
+                                                    </Chip>
+                                                })}
+                                            </Box>
+                                        </Stack>
+                                    </React.Fragment>
+                                })}
+                        </Stack>
+                    </Box>
+                </ModalDialogExtended>
+            )}
+        />
+    )
 }
