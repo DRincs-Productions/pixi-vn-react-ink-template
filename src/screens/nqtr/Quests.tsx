0488e6ec41f3d6785d4306adde813e5c94cafac9
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-06-22 19:12:51 +0200

    Quests

--- src/screens/nqtr/Quests.tsx
+++ src/screens/nqtr/Quests.tsx
@@ -1,15 +1,18 @@
-import { Drawer, ModalDialogExtended, useThemeMaterial } from '@drincs/react-components';
-import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import { Input, Stack, Typography } from "@mui/joy";
-import { useEffect, useState } from 'react';
+import { Box, ModalDialogExtended, Sheet } from '@drincs/react-components';
+import { AspectRatio, Divider, Stack, Typography } from "@mui/joy";
+import { useEffect } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { useRecoilState } from 'recoil';
-import { openHistoryState } from '../../atoms/openHistoryState';
 
 export default function Quest() {
-    const [open, setOpen] = useRecoilState(openHistoryState);
-    const [searchString, setSearchString] = useState("")
     const { t } = useTranslation(["translation"]);
+    const methods = useForm<{
+        open: boolean,
+    }>({
+        defaultValues: {
+            open: false,
+        }
+    });
 
     useEffect(() => {
         window.addEventListener('keydown', onkeydown);
@@ -20,43 +23,113 @@ export default function Quest() {
 
     function onkeydown(event: KeyboardEvent) {
         if (event.code == 'KeyJ') {
-            setOpen((prev) => !prev)
+            let open = methods.getValues("open")
+            methods.setValue("open", !open)
         }
     }
 
     return (
-        <ModalDialogExtended
-            open={open}
-            setOpen={setOpen}
-            head={<Stack
-                sx={{
-                    width: "100%",
-                }}
-            >
-                <Stack sx={{ mb: 2 }}>
-                    <Typography level="h2">
-                        {t("history")}
-                    </Typography>
-                </Stack>
-                <Input
-                    placeholder={t("search")}
-                    value={searchString}
-                    onChange={(e) => setSearchString(e.target.value)}
-                    startDecorator={<SearchRoundedIcon />}
-                    aria-label={t("search")}
-                />
-            </Stack>}
-            minWidth="80%"
-            sx={{
-                maxHeight: "80%",
-            }}
-        >
-            <Drawer
-                zIndex={useThemeMaterial().zIndex.drawer}
-                width={200}
-            >
-
-            </Drawer>
-        </ModalDialogExtended>
+        <Controller
+            control={methods.control}
+            name="open"
+            render={({ field: { value: open } }) => (
+                <ModalDialogExtended
+                    open={open}
+                    setOpen={(value) => methods.setValue("open", value)}
+                    head={<Stack
+                        sx={{
+                            width: "100%",
+                        }}
+                    >
+                        <Stack sx={{ mb: 2 }}>
+                            <Typography level="h2">
+                                {t("quests")}
+                            </Typography>
+                        </Stack>
+                    </Stack>}
+                    minWidth="80%"
+                    sx={{
+                        maxHeight: "100%",
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            minHeight: '100%',
+                        }}
+                    >
+                        <Sheet
+                            className="Sidebar"
+                            sx={{
+                                position: 'sticky',
+                                transition: 'transform 0.4s, width 0.4s',
+                                width: 200,
+                                top: 0,
+                                p: 2,
+                                flexShrink: 0,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                gap: 2,
+                            }}
+                        >
+                            <Box>
+                                dwqe
+                            </Box>
+                        </Sheet>
+                        <Sheet
+                            component="main"
+                            className="MainContent"
+                            sx={{
+                                flex: 1,
+                                display: "flex",
+                                flexDirection: "column",
+                                minWidth: 0,
+                                gap: 1,
+                                overflow: "auto",
+                                p: 5,
+                            }}
+                        >
+                            <Stack
+                                spacing={1}
+                            >
+                                <AspectRatio
+                                    maxHeight={"10dvh"}
+                                >
+                                    <img
+                                        src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800"
+                                        srcSet="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800&dpr=2 2x"
+                                        alt=""
+                                    />
+                                </AspectRatio>
+                                <Typography
+                                    level="h2"
+                                    textAlign={"center"}
+                                    maxHeight={"10dvh"}
+                                    sx={{
+                                        overflowY: "auto",
+                                    }}
+                                >
+                                </Typography>
+                                <Typography
+                                    maxHeight={"20dvh"}
+                                    sx={{
+                                        overflowY: 'scroll',
+                                    }}
+                                >
+                                </Typography>
+                                <Divider />
+                                <Typography
+                                    maxHeight={"20dvh"}
+                                    sx={{
+                                        overflowY: 'scroll',
+                                    }}
+                                >
+                                </Typography>
+                            </Stack>
+                        </Sheet>
+                    </Box>
+                </ModalDialogExtended>
+            )}
+        />
     );
 }
